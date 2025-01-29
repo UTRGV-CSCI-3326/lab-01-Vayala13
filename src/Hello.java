a50b093c720fab66a018f533dc7527359aad4034
Viviana Ayala <ayalav@myyahoo.com>
2025-01-29 14:09:14 +0000

    V's First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,7 @@
-// Hello World
+public class Hello {
+    //Creating our work area
+    public static void main(String[] args) {
+    //how to pring hello world in JAVA  
+        System.out.println("Hello World!");
+    }
+}
